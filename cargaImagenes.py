18cbb1e905385ceeee2b4aa48accb3ca749989a3
a01751685 <A01751685@itesm.mx>
2021-03-19 10:06:33 -0600

    n archivos .py

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 
 #Cargar la imagen a color
-IRGB=cv2.imread('002.jpg')
+IRGB=cv2.imread('imagen.jpg')
 print(IRGB)
 print(IRGB.shape)
 print("Lineas agregadas en la rama2")
 IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
 print(IGS)
 print(IGS.shape)
-cv2.imwrite('002GS.jpg',IGS)
+cv2.imwrite('imagenGS.jpg',IGS)
 
